bc576e6d891539a0bdda72cba99f2bc4256d9962
Lucho Celentano <chiquicelentano@gmail.com>
2022-06-14 22:49:02 -0300

    Se creo Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+   <header className='navbar'>
+    <img src="" alt="Lc3d" className='logo'></img>
+ <nav>
+    <ul>
+        <li> <a>Inicio</a></li>
+        <li> <a>Nosotros</a></li>
+        <li> <a>Modelado</a></li>
+        <li> <a>Contacto</a></li>
+    </ul>
+
+    </nav> 
+   
+   </header>
+
+  );
+}
+
+export default Navbar;
